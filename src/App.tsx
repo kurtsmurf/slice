78474c6f75a0c9de80df124e680209a1ffbeb69b
Kurt <chrisronsmith@gmail.com>
2023-04-07 18:21:20 -0400

    use bars

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { createVirtualizer } from "@tanstack/solid-virtual";
 
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 100;
-const SAMPLES_PER_PX = 100;
+// todo: make dynamic with min 1 max 1000, integers only
+const SAMPLES_PER_PX = 20;
 
 export const App = () => {
   const [clip, setClip] = createSignal<Clip | undefined>();
@@ -38,7 +39,8 @@ const Waveform = (props: { clip: Clip }) => {
   let root: HTMLDivElement | undefined;
 
   const tileManager = createVirtualizer({
-    count: range(0, props.clip.buffer.length / SAMPLES_PER_PX, CANVAS_WIDTH).length,
+    count:
+      range(0, props.clip.buffer.length / SAMPLES_PER_PX, CANVAS_WIDTH).length,
     getScrollElement: () => root,
     estimateSize: () => CANVAS_WIDTH,
     horizontal: true,
@@ -102,11 +104,7 @@ const ChannelSegment = (
 
   onMount(() => {
     if (!canvas) return;
-    if (SAMPLES_PER_PX <= 1) {
-      drawPath(canvas, props.channelData, props.start, props.length);
-    } else {
-      drawBars(canvas, props.channelData, props.start, props.length);
-    }
+    drawBars(canvas, props.channelData, props.start, props.length);
   });
 
   return (
@@ -119,68 +117,35 @@ const ChannelSegment = (
 const range = (start: number, end: number, step = 1) =>
   [...new Array(Math.ceil((end - start) / step))].map((_, i) => i * step);
 
-const drawBars = (canvas: HTMLCanvasElement, channelData: Float32Array, start: number, length: number) => {
+const drawBars = (
+  canvas: HTMLCanvasElement,
+  channelData: Float32Array,
+  start: number,
+  length: number,
+) => {
   const context = canvas.getContext("2d");
   if (!context) return;
+  const LINE_WIDTH = 2;
 
   // shift origin
   context.translate(0, canvas.height / 2);
-
-  // draw baseline
-  context.beginPath();
-  context.lineWidth = 1;
-  context.setLineDash([2]);
-  context.moveTo(0, 0);
-  context.lineTo(canvas.width, 0);
-  context.stroke();
-  context.setLineDash([]);
-  context.closePath();
+  context.lineWidth = LINE_WIDTH;
 
   // draw waveform
   let bucket: number[] = [];
-  context.lineWidth = 1.5;
-
   for (let i = 0; i < length; i++) {
-    bucket.push(channelData[i + start])
+    bucket.push(channelData[i + start]);
     if (bucket.length === SAMPLES_PER_PX) {
-      const min = Math.min(...bucket) * (canvas.height / 2)
-      const max = Math.max(...bucket) * (canvas.height / 2)
+      const min = Math.min(...bucket) * (canvas.height / 2);
+      const max = Math.max(...bucket) * (canvas.height / 2);
       bucket = [];
       // draw line from window min to window max
       // along the y axis
       // at x = i
-      context.beginPath()
-      context.moveTo(i / SAMPLES_PER_PX, max + 1)
-      context.lineTo(i / SAMPLES_PER_PX, min - 1)
+      context.beginPath();
+      context.moveTo(i / SAMPLES_PER_PX, max + LINE_WIDTH / 2);
+      context.lineTo(i / SAMPLES_PER_PX, min - LINE_WIDTH / 2);
       context.stroke();
     }
   }
-}
-
-const drawPath = (canvas: HTMLCanvasElement, channelData: Float32Array, start: number, length: number) => {
-  const context = canvas.getContext("2d");
-  if (!context) return;
-
-  // shift origin
-  context.translate(0, canvas.height / 2);
-
-  // draw baseline
-  context.beginPath();
-  context.lineWidth = 1;
-  context.setLineDash([2]);
-  context.moveTo(0, 0);
-  context.lineTo(canvas.width, 0);
-  context.stroke();
-  context.setLineDash([]);
-  context.closePath();
-
-  // draw waveform
-  context.beginPath();
-  context.lineWidth = 2;
-  const overlap = 5;
-  for (let i = -overlap; i < length + overlap; i++) {
-    context.lineTo(i / SAMPLES_PER_PX, channelData[start + i] * canvas.height / 2);
-  }
-  context.stroke();
-  context.closePath();
-}
+};
