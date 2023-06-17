7cf87f1727a8df1268b0710f3be32f400ebd8c3f
Chris Smith <chrisronsmith@gmail.com>
2023-06-17 13:47:31 -0400

    deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { qrcode } from 'vite-plugin-qrcode';
-import { VitePWA } from 'vite-plugin-pwa'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+  base: '/slice/',
   plugins: [
     solidPlugin(),
     qrcode(),
