a3529c24ee958baf0fd1b4fc86ec0baecbf85333
Kurt <chrisronsmith@gmail.com>
2023-06-22 11:28:15 -0400

    sort flags

--- src/App.tsx
+++ src/App.tsx
@@ -170,7 +170,11 @@ const Controls = (props: { clip: Clip }) => (
     </button>
     <button
       onClick={() => {
-        setFlags((prev) => [...prev, cursor()]);
+        setFlags((prev) => {
+          const i = sortedIndex(prev, cursor());
+          if (prev[i] === cursor()) return prev;
+          return [...prev.slice(0, i), cursor(), ...prev.slice(i)];
+        });
       }}
     >
       drop a flag
@@ -598,4 +602,17 @@ const drawBars = (
 
   // shift origin back
   context.translate(0, -canvas.height / 2);
+};
+
+const sortedIndex = (arr: number[], value: number) => {
+  let low = 0;
+  let high = arr.length;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (arr[mid] < value) low = mid + 1;
+    else if (arr[mid] > value) high = mid - 1;
+    else return mid;
+  }
+  return low;
 };
