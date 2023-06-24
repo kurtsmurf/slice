1e30b2905c189f4d9c39f1f1a246c19b4f33eaf3
Kurt <chrisronsmith@gmail.com>
2023-06-24 17:15:35 -0400

    screenshot test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -2,22 +2,28 @@ import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("localhost:3000");
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/slice/i);
 });
 
-test("loads clip", async ({ page }) => {
+test("loads clip", async ({ page }, testInfo) => {
   await page.goto("localhost:3000");
   await page.evaluate(() => {
+    // set clip to a 60s buffer of silence named hello
     // @ts-ignore
     window.setClip({
       name: "hello",
       buffer: new AudioBuffer({
         sampleRate: 44100,
         length: 44100 * 60,
-      })
-    })
-  })
-  // await page.pause();
-})
+      }),
+    });
+  });
+  // does not wait for async canvas rendering to finish
+  await testInfo.attach(
+    "screenshot",
+    {
+      body: await page.screenshot(),
+      contentType: "image/png",
+    },
+  );
+});
